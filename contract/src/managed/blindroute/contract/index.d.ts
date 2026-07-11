1c7268208a65b2c564fa5b7952eac2a933626869
Jrabara101 <jrabara101@gmail.com>
2026-07-11 20:47:32 +0800

    build: add generated managed/ circuits and keys
    Output of `compact compile src/blindroute.compact src/managed/blindroute`
    (compact compiler 0.31.1): compiled contract bindings, zkIR, and
    prover/verifier keys for the lockEscrow and releaseEscrow circuits.

---
+++ contract/src/managed/blindroute/contract/index.d.ts
@@ -0,0 +1,62 @@
+import type * as __compactRuntime from '@midnight-ntwrk/compact-runtime';
+
+export enum EscrowState { EMPTY = 0, LOCKED = 1, RELEASED = 2 }
+
+export type Witnesses<PS> = {
+  localSecretKey(context: __compactRuntime.WitnessContext<Ledger, PS>): [PS, Uint8Array];
+  deliveryProof(context: __compactRuntime.WitnessContext<Ledger, PS>): [PS, Uint8Array];
+}
+
+export type ImpureCircuits<PS> = {
+  lockEscrow(context: __compactRuntime.CircuitContext<PS>,
+             paymentAmount_0: bigint,
+             commitment_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+  releaseEscrow(context: __compactRuntime.CircuitContext<PS>): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type ProvableCircuits<PS> = {
+  lockEscrow(context: __compactRuntime.CircuitContext<PS>,
+             paymentAmount_0: bigint,
+             commitment_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+  releaseEscrow(context: __compactRuntime.CircuitContext<PS>): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type PureCircuits = {
+  commitmentOf(proof_0: Uint8Array): Uint8Array;
+  publicKey(sk_0: Uint8Array, seq_0: Uint8Array): Uint8Array;
+}
+
+export type Circuits<PS> = {
+  lockEscrow(context: __compactRuntime.CircuitContext<PS>,
+             paymentAmount_0: bigint,
+             commitment_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+  releaseEscrow(context: __compactRuntime.CircuitContext<PS>): __compactRuntime.CircuitResults<PS, []>;
+  commitmentOf(context: __compactRuntime.CircuitContext<PS>, proof_0: Uint8Array): __compactRuntime.CircuitResults<PS, Uint8Array>;
+  publicKey(context: __compactRuntime.CircuitContext<PS>,
+            sk_0: Uint8Array,
+            seq_0: Uint8Array): __compactRuntime.CircuitResults<PS, Uint8Array>;
+}
+
+export type Ledger = {
+  readonly state: EscrowState;
+  readonly amount: bigint;
+  readonly deliveryCommitment: Uint8Array;
+  readonly courier: Uint8Array;
+  readonly sequence: bigint;
+}
+
+export type ContractReferenceLocations = any;
+
+export declare const contractReferenceLocations : ContractReferenceLocations;
+
+export declare class Contract<PS = any, W extends Witnesses<PS> = Witnesses<PS>> {
+  witnesses: W;
+  circuits: Circuits<PS>;
+  impureCircuits: ImpureCircuits<PS>;
+  provableCircuits: ProvableCircuits<PS>;
+  constructor(witnesses: W);
+  initialState(context: __compactRuntime.ConstructorContext<PS>): __compactRuntime.ConstructorResult<PS>;
+}
+
+export declare function ledger(state: __compactRuntime.StateValue | __compactRuntime.ChargedState): Ledger;
+export declare const pureCircuits: PureCircuits;
